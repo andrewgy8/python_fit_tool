12164e0e8a9af15cb986bbf1927ed2784c0d4ba8
Andrew Graham-Yooll <andrewgy8@gmail.com>
2025-07-23 21:31:51 +0200

    Update all tests to use pytest. Use uv packaging

--- fit_tool/tests/test_fit_file_header.py
+++ fit_tool/tests/test_fit_file_header.py
@@ -1,61 +1,42 @@
 # nosetests --nocapture  tests/test_fit_file_header.py
 
-import unittest
-
 from fit_tool.fit_file_header import ProtocolVersion, ProfileVersion, FitFileHeader
 
+def test_protocol_version():
+    protocol_version = ProtocolVersion(2, 3)
+    assert str(protocol_version) == '2.3'
+    bytes1 = protocol_version.to_bytes()
+    pv2 = ProtocolVersion.from_bytes(bytes1)
+    bytes2 = pv2.to_bytes()
+    assert bytes1 == bytes2
 
-class TestFitFileHeader(unittest.TestCase):
+def test_profile_version():
+    profile_version = ProfileVersion(21, 60)
+    assert str(profile_version) == '21.60'
+    bytes1 = profile_version.to_bytes()
+    pv2 = ProfileVersion.from_bytes(bytes1)
+    bytes2 = pv2.to_bytes()
+    assert bytes1 == bytes2
 
-    def shortDescription(self):
-        return None
+def test_generate_crc():
+    protocol_version = ProtocolVersion(2, 3)
+    profile_version = ProfileVersion(21, 60)
+    FitFileHeader.generate_crc(protocol_version, profile_version, 6552343)
 
-    def test_protocol_version(self):
-        protocol_version = ProtocolVersion(2, 3)
-        self.assertEqual('2.3', str(protocol_version))
+def test_conversion_without_crc():
+    header1 = FitFileHeader(protocol_version=ProtocolVersion(2, 3), profile_version=ProfileVersion(21, 60),
+                           records_size=4294967295)
+    bytes1 = header1.to_bytes()
+    assert len(bytes1) == 12
+    header2 = FitFileHeader.from_bytes(bytes1)
+    bytes2 = header2.to_bytes()
+    assert bytes1 == bytes2
 
-        bytes1 = protocol_version.to_bytes()
-
-        pv2 = ProtocolVersion.from_bytes(bytes1)
-        bytes2 = pv2.to_bytes()
-
-        self.assertEqual(bytes1, bytes2)
-
-    def test_profile_version(self):
-        profile_version = ProfileVersion(21, 60)
-        self.assertEqual('21.60', str(profile_version))
-
-        bytes1 = profile_version.to_bytes()
-
-        pv2 = ProfileVersion.from_bytes(bytes1)
-        bytes2 = pv2.to_bytes()
-
-        self.assertEqual(bytes1, bytes2)
-
-    def test_generate_crc(self):
-        protocol_version = ProtocolVersion(2, 3)
-        profile_version = ProfileVersion(21, 60)
-
-        FitFileHeader.generate_crc(protocol_version, profile_version, 6552343)
-
-    def test_conversion_without_crc(self):
-        header1 = FitFileHeader(protocol_version=ProtocolVersion(2, 3), profile_version=ProfileVersion(21, 60),
-                                records_size=4294967295)
-        bytes1 = header1.to_bytes()
-        self.assertEqual(12, len(bytes1))
-
-        header2 = FitFileHeader.from_bytes(bytes1)
-        bytes2 = header2.to_bytes()
-
-        self.assertEqual(bytes1, bytes2)
-
-    def test_conversion_with_crc(self):
-        header1 = FitFileHeader(protocol_version=ProtocolVersion(2, 3), profile_version=ProfileVersion(21, 60),
-                                records_size=4294967295, crc=500)
-        bytes1 = header1.to_bytes()
-        self.assertEqual(14, len(bytes1))
-
-        header2 = FitFileHeader.from_bytes(bytes1)
-        bytes2 = header2.to_bytes()
-
-        self.assertEqual(bytes1, bytes2)
+def test_conversion_with_crc():
+    header1 = FitFileHeader(protocol_version=ProtocolVersion(2, 3), profile_version=ProfileVersion(21, 60),
+                           records_size=4294967295, crc=500)
+    bytes1 = header1.to_bytes()
+    assert len(bytes1) == 14
+    header2 = FitFileHeader.from_bytes(bytes1)
+    bytes2 = header2.to_bytes()
+    assert bytes1 == bytes2
--- fit_tool/tests/test_sdk_files.py
+++ fit_tool/tests/test_sdk_files.py
@@ -1,197 +1,118 @@
-# nosetests --nocapture  tests/test_field.py
 import csv
 import os
-import unittest
-
 from fit_tool.fit_file import FitFile
 
+def test_developer_data():
+    path = os.path.join(os.path.dirname(__file__), 'data/sdk/DeveloperData.fit')
+    with open(path, 'rb') as file_object:
+        bytes_buffer = file_object.read()
+        fit_file = FitFile.from_bytes(bytes_buffer)
+        fit_file.to_rows()
 
-class TestSdkFiles(unittest.TestCase):
+def test_activity(tmpdir):
+    path = os.path.join(os.path.dirname(__file__), 'data/sdk/Activity.fit')
+    with open(path, 'rb') as file_object:
+        bytes_buffer = file_object.read()
+        fit_file = FitFile.from_bytes(bytes_buffer)
+        rows = fit_file.to_rows()
+    out_path = tmpdir / 'Activity.csv'
+    with open(out_path, 'w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for row in rows:
+            csv_writer.writerow(row)
 
-    def setUp(self):
-        super().setUp()
-        # stream_handler = logging.StreamHandler(sys.stdout)
-        # stream_handler.setFormatter(formatter)
-        # logger.addHandler(stream_handler)
+def test_activity_developer_data(tmpdir):
+    path = os.path.join(os.path.dirname(__file__), 'data/sdk/activity_developerdata.fit')
+    with open(path, 'rb') as file_object:
+        bytes_buffer = file_object.read()
+        fit_file = FitFile.from_bytes(bytes_buffer)
+        rows = fit_file.to_rows()
+    out_path = tmpdir / 'activity_developerdata.csv'
+    with open(out_path, 'w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for row in rows:
+            csv_writer.writerow(row)
 
-    def shortDescription(self):
-        return None
+def test_activity_low_battery():
+    path = os.path.join(os.path.dirname(__file__), 'data/sdk/activity_lowbattery.fit')
+    with open(path, 'rb') as file_object:
+        bytes_buffer = file_object.read()
+        fit_file = FitFile.from_bytes(bytes_buffer)
+        fit_file.to_rows()
 
-    def test_developer_data(self):
-        """Test decoding activity file with developer fields.
-        """
-        path = os.path.join(os.path.dirname(__file__), 'data/sdk/DeveloperData.fit')
+def test_activity_multi_sport():
+    path = os.path.join(os.path.dirname(__file__), 'data/sdk/activity_multisport.fit')
+    with open(path, 'rb') as file_object:
+        bytes_buffer = file_object.read()
+        fit_file = FitFile.from_bytes(bytes_buffer)
+        fit_file.to_rows()
 
-        with open(path, 'rb') as file_object:
-            bytes_buffer = file_object.read()
-            fit_file = FitFile.from_bytes(bytes_buffer)
-            # print(f'Profile version: {fit_file.header.profile_version}')
-            fit_file.to_rows()
+def test_activity_poolswim_with_hr():
+    path = os.path.join(os.path.dirname(__file__), 'data/sdk/activity_poolswim_with_hr.fit')
+    with open(path, 'rb') as file_object:
+        bytes_buffer = file_object.read()
+        fit_file = FitFile.from_bytes(bytes_buffer)
+        fit_file.to_rows()
 
-    def test_activity(self):
-        """Test decoding activity file.
-        """
-        path = os.path.join(os.path.dirname(__file__), 'data/sdk/Activity.fit')
+def test_activity_poolswim():
+    path = os.path.join(os.path.dirname(__file__), 'data/sdk/activity_poolswim.fit')
+    with open(path, 'rb') as file_object:
+        bytes_buffer = file_object.read()
+        fit_file = FitFile.from_bytes(bytes_buffer)
+        fit_file.to_rows()
 
-        with open(path, 'rb') as file_object:
-            bytes_buffer = file_object.read()
-            fit_file = FitFile.from_bytes(bytes_buffer)
-            # print(f'Profile version: {fit_file.header.profile_version}')
-            rows = fit_file.to_rows()
+def test_activity_truncated():
+    path = os.path.join(os.path.dirname(__file__), 'data/sdk/activity_truncated.fit')
+    with open(path, 'rb') as file_object:
+        bytes_buffer = file_object.read()
+        fit_file = FitFile.from_bytes(bytes_buffer)
+        fit_file.to_rows()
 
-        out_path = os.path.join(os.path.dirname(__file__), 'out/Activity.csv')
-        with open(out_path, 'w', newline='') as csv_file:
-            csv_writer = csv.writer(csv_file, delimiter=',',
-                                    quotechar='|', quoting=csv.QUOTE_MINIMAL)
-            for row in rows:
-                csv_writer.writerow(row)
+def test_monitoring_file():
+    path = os.path.join(os.path.dirname(__file__), 'data/sdk/MonitoringFile.fit')
+    with open(path, 'rb') as file_object:
+        bytes_buffer = file_object.read()
+        fit_file = FitFile.from_bytes(bytes_buffer)
+        fit_file.to_rows()
 
-    def test_activity_developer_data(self):
-        """Test decoding activity file.
-        """
-        path = os.path.join(os.path.dirname(__file__), 'data/sdk/activity_developerdata.fit')
+def test_weight_scale_multi_user():
+    path = os.path.join(os.path.dirname(__file__), 'data/sdk/WeightScaleMultiUser.fit')
+    with open(path, 'rb') as file_object:
+        bytes_buffer = file_object.read()
+        fit_file = FitFile.from_bytes(bytes_buffer)
+        fit_file.to_rows()
 
-        with open(path, 'rb') as file_object:
-            bytes_buffer = file_object.read()
-            fit_file = FitFile.from_bytes(bytes_buffer)
-            # print(f'Profile version: {fit_file.header.profile_version}')
-            rows = fit_file.to_rows()
+def test_weight_scale_single_user():
+    path = os.path.join(os.path.dirname(__file__), 'data/sdk/WeightScaleSingleUser.fit')
+    with open(path, 'rb') as file_object:
+        bytes_buffer = file_object.read()
+        fit_file = FitFile.from_bytes(bytes_buffer)
+        fit_file.to_rows()
 
-        out_path = os.path.join(os.path.dirname(__file__), 'out/activity_developerdata.csv')
-        with open(out_path, 'w', newline='') as csv_file:
-            csv_writer = csv.writer(csv_file, delimiter=',',
-                                    quotechar='|', quoting=csv.QUOTE_MINIMAL)
-            for row in rows:
-                csv_writer.writerow(row)
+def test_workout_individual_steps():
+    path = os.path.join(os.path.dirname(__file__), 'data/sdk/WorkoutIndividualSteps.fit')
+    with open(path, 'rb') as file_object:
+        bytes_buffer = file_object.read()
+        fit_file = FitFile.from_bytes(bytes_buffer)
+        fit_file.to_rows()
 
-    def test_activity_low_battery(self):
-        """Test decoding activity low battery file.
-        """
-        path = os.path.join(os.path.dirname(__file__), 'data/sdk/activity_lowbattery.fit')
+def test_workout_custom_target_values():
+    path = os.path.join(os.path.dirname(__file__), 'data/sdk/WorkoutCustomTargetValues.fit')
+    with open(path, 'rb') as file_object:
+        bytes_buffer = file_object.read()
+        fit_file = FitFile.from_bytes(bytes_buffer)
+        fit_file.to_rows()
 
-        with open(path, 'rb') as file_object:
-            bytes_buffer = file_object.read()
-            fit_file = FitFile.from_bytes(bytes_buffer)
-            # print(f'Profile version: {fit_file.header.profile_version}')
-            fit_file.to_rows()
+def test_workout_repeat_greater_than_step():
+    path = os.path.join(os.path.dirname(__file__), 'data/sdk/WorkoutRepeatGreaterThanStep.fit')
+    with open(path, 'rb') as file_object:
+        bytes_buffer = file_object.read()
+        fit_file = FitFile.from_bytes(bytes_buffer)
+        fit_file.to_rows()
 
-    def test_activity_multi_sport(self):
-        """Test decoding activity multisport file.
-        """
-        path = os.path.join(os.path.dirname(__file__), 'data/sdk/activity_multisport.fit')
-
-        with open(path, 'rb') as file_object:
-            bytes_buffer = file_object.read()
-            fit_file = FitFile.from_bytes(bytes_buffer)
-            # print(f'Profile version: {fit_file.header.profile_version}')
-            fit_file.to_rows()
-
-    def test_activity_poolswim_with_hr(self):
-        """Test decoding
-        """
-        path = os.path.join(os.path.dirname(__file__), 'data/sdk/activity_poolswim_with_hr.fit')
-
-        with open(path, 'rb') as file_object:
-            bytes_buffer = file_object.read()
-            fit_file = FitFile.from_bytes(bytes_buffer)
-            # print(f'Profile version: {fit_file.header.profile_version}')
-            fit_file.to_rows()
-
-    def test_activity_poolswim(self):
-        """Test decoding
-        """
-        path = os.path.join(os.path.dirname(__file__), 'data/sdk/activity_poolswim.fit')
-
-        with open(path, 'rb') as file_object:
-            bytes_buffer = file_object.read()
-            fit_file = FitFile.from_bytes(bytes_buffer)
-            # print(f'Profile version: {fit_file.header.profile_version}')
-            fit_file.to_rows()
-
-    def test_activity_truncated(self):
-        """Test decoding
-        """
-        path = os.path.join(os.path.dirname(__file__), 'data/sdk/activity_truncated.fit')
-
-        with open(path, 'rb') as file_object:
-            bytes_buffer = file_object.read()
-            fit_file = FitFile.from_bytes(bytes_buffer)
-            # print(f'Profile version: {fit_file.header.profile_version}')
-            fit_file.to_rows()
-
-    def test_monitoring_file(self):
-        """Test decoding
-        """
-        path = os.path.join(os.path.dirname(__file__), 'data/sdk/MonitoringFile.fit')
-
-        with open(path, 'rb') as file_object:
-            bytes_buffer = file_object.read()
-            fit_file = FitFile.from_bytes(bytes_buffer)
-            # print(f'Profile version: {fit_file.header.profile_version}')
-            fit_file.to_rows()
-
-    def test_weight_scale_multi_user(self):
-        """Test decoding
-        """
-        path = os.path.join(os.path.dirname(__file__), 'data/sdk/WeightScaleMultiUser.fit')
-
-        with open(path, 'rb') as file_object:
-            bytes_buffer = file_object.read()
-            fit_file = FitFile.from_bytes(bytes_buffer)
-            # print(f'Profile version: {fit_file.header.profile_version}')
-            fit_file.to_rows()
-
-    def test_weight_scale_single_user(self):
-        """Test decoding
-        """
-        path = os.path.join(os.path.dirname(__file__), 'data/sdk/WeightScaleSingleUser.fit')
-
-        with open(path, 'rb') as file_object:
-            bytes_buffer = file_object.read()
-            fit_file = FitFile.from_bytes(bytes_buffer)
-            # print(f'Profile version: {fit_file.header.profile_version}')
-            fit_file.to_rows()
-
-    def test_workout_individual_steps(self):
-        """Test decoding
-        """
-        path = os.path.join(os.path.dirname(__file__), 'data/sdk/WorkoutIndividualSteps.fit')
-
-        with open(path, 'rb') as file_object:
-            bytes_buffer = file_object.read()
-            fit_file = FitFile.from_bytes(bytes_buffer)
-            # print(f'Profile version: {fit_file.header.profile_version}')
-            fit_file.to_rows()
-
-    def test_workout_custom_target_values(self):
-        """Test decoding
-        """
-        path = os.path.join(os.path.dirname(__file__), 'data/sdk/WorkoutCustomTargetValues.fit')
-
-        with open(path, 'rb') as file_object:
-            bytes_buffer = file_object.read()
-            fit_file = FitFile.from_bytes(bytes_buffer)
-            # print(f'Profile version: {fit_file.header.profile_version}')
-            fit_file.to_rows()
-
-    def test_workout_repeat_greater_than_step(self):
-        """Test decoding
-        """
-        path = os.path.join(os.path.dirname(__file__), 'data/sdk/WorkoutRepeatGreaterThanStep.fit')
-
-        with open(path, 'rb') as file_object:
-            bytes_buffer = file_object.read()
-            fit_file = FitFile.from_bytes(bytes_buffer)
-            # print(f'Profile version: {fit_file.header.profile_version}')
-            fit_file.to_rows()
-
-    def test_workout_repeat_steps(self):
-        """Test decoding workout repeat steps file.
-        """
-        path = os.path.join(os.path.dirname(__file__), 'data/sdk/WorkoutRepeatSteps.fit')
-
-        with open(path, 'rb') as file_object:
-            bytes_buffer = file_object.read()
-            fit_file = FitFile.from_bytes(bytes_buffer)
-            # print(f'Profile version: {fit_file.header.profile_version}')
-            fit_file.to_rows()
+def test_workout_repeat_steps():
+    path = os.path.join(os.path.dirname(__file__), 'data/sdk/WorkoutRepeatSteps.fit')
+    with open(path, 'rb') as file_object:
+        bytes_buffer = file_object.read()
+        fit_file = FitFile.from_bytes(bytes_buffer)
+        fit_file.to_rows()
